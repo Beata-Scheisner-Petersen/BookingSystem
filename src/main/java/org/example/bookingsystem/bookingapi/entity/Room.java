fd0deae0ddf43f68f9c0cf08d49372747fbef5d6
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-16 02:49:08 +0200

    Add: Error handling and a constructor

--- src/main/java/org/example/bookingsystem/bookingapi/entity/Room.java
+++ src/main/java/org/example/bookingsystem/bookingapi/entity/Room.java
@@ -1,6 +1,7 @@
 package org.example.bookingsystem.bookingapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 @Table(name = "room")
@@ -11,13 +12,22 @@ public class Room {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "room_number", nullable = false)
+    @Column(name = "room_number", nullable = false, unique = true)
+    @Positive(message = "Room number can't be 0 or Negative")
     private int roomNumber;
 
     @Column(name = "room_size", nullable = false)
+    @Positive(message = "Room size can't be 0 or Negative")
     private int roomSize;
 
+    public Room(int roomNumber, int roomSize) {
+        this.roomNumber = roomNumber;
+        this.roomSize = roomSize;
+    }
 
+    public Room() {
+
+    }
 
     public Long getId() {
         return id;
