50d11d1627cae98bbc4b4076276537a83f2bba66
soppitopp <soppitopp2000@hotmail.com>
2026-05-17 13:40:07 +0200

    add: GlobalExceptionHandler.java

---
+++ src/main/java/org/example/bookingsystem/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.example.bookingsystem.exceptionhandler;
+
+//import org.example.mynotes2.exceptionhandler.customexceptions.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationError(
+            MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+//    @ExceptionHandler(UsernameAlreadyExistsException.class)
+//    public ResponseEntity<String> handleUsernameExists(UsernameAlreadyExistsException e) {
+//        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+//    }
+}
