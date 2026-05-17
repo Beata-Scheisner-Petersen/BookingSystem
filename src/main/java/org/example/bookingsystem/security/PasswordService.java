43aac4d11cfbad946fe7e0393d4cafa6de89cc5d
soppitopp <soppitopp2000@hotmail.com>
2026-05-17 13:40:58 +0200

    add: PasswordService.java + hash() + matches()

---
+++ src/main/java/org/example/bookingsystem/security/PasswordService.java
@@ -0,0 +1,15 @@
+package org.example.bookingsystem.security;
+
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.stereotype.*;
+
+@Service
+public class PasswordService {
+    public String hash(String rawPassword) {
+        return BCrypt.hashpw(rawPassword, BCrypt.gensalt());
+    }
+
+    public boolean matches(String rawPassword, String hashedPassword) {
+        return BCrypt.checkpw(rawPassword, hashedPassword);
+    }
+}
