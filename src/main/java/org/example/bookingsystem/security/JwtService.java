d596c291e23de9ad366d1b5d62eb6f87a374e378
soppitopp <soppitopp2000@hotmail.com>
2026-05-19 17:38:41 +0200

    cmmoit for Tokens for Daniel

---
+++ src/main/java/org/example/bookingsystem/security/JwtService.java
@@ -0,0 +1,33 @@
+package org.example.bookingsystem.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.*;
+import org.springframework.beans.factory.annotation.*;
+
+import java.security.*;
+import java.util.*;
+public class JwtService {
+    private final Key key;
+
+    public JwtService(@Value("$jwt.secret") String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generatedToken(Long CustomerId, String email) {
+        return Jwts.builder()
+                .setSubject(CustomerId.toString())
+                .claim("email", email)
+                .setIssuedAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // giltighetstid 1h
+                .signWith(key, SignatureAlgorithm.ES512)
+                .compact();
+    }
+
+    public Long extractCustomerId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.parseLong(claims.getSubject());
+    }
+}
