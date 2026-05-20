8e334222d6f419067522f9277ffbc1df0e333a90
soppitopp <soppitopp2000@hotmail.com>
2026-05-20 11:08:20 +0200

    removed @column

--- src/main/java/org/example/bookingsystem/customer/model/dto/CustomerUpdateRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CustomerUpdateRequest.java
@@ -3,7 +3,7 @@ package org.example.bookingsystem.customer.model.dto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 public record CustomerUpdateRequest(
-        @Column(name = "email", unique = true)
+
         @NotBlank(message = "You must enter a email.")
         @Email(message = "Email format is invalid.")
         String email,
@@ -11,7 +11,7 @@ public record CustomerUpdateRequest(
         @NotBlank(message = "You must enter a password.")
         String password,
 
-        @Column(name = "phone_number", unique = true)
+        
         String phoneNumber
 ) {
 }
