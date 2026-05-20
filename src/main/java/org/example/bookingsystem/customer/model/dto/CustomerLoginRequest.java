5c24f55d708f23b9cafa6f7b558772b667ea4401
soppitopp <soppitopp2000@hotmail.com>
2026-05-20 11:11:28 +0200

    add space

--- src/main/java/org/example/bookingsystem/customer/model/dto/CustomerLoginRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CustomerLoginRequest.java
@@ -2,6 +2,7 @@ package org.example.bookingsystem.customer.model.dto;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+
 public record CustomerLoginRequest(
         @Column(name = "email", unique = true)
         @NotBlank(message = "You must enter a email.")
