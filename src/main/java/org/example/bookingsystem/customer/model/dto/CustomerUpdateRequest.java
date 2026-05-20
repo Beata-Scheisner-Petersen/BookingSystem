2106e0a87fb8b62d0a7bf0516bbe96aacf67b0aa
soppitopp <soppitopp2000@hotmail.com>
2026-05-20 19:52:07 +0200

    Fix CustomerUpdateRequest validation of variables

--- src/main/java/org/example/bookingsystem/customer/model/dto/CustomerUpdateRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CustomerUpdateRequest.java
@@ -4,14 +4,17 @@ import jakarta.validation.constraints.*;
 
 public record CustomerUpdateRequest(
 
-        @NotBlank(message = "You must enter a email.")
         @Email(message = "Email format is invalid.")
         String email,
 
-        @NotBlank(message = "You must enter a password.")
+        @Size(min = 10)
+        @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "Password have to contain a least one special character")
+        @Pattern(regexp = ".*[A-Z].*", message = "Password have to contain a least one uppercase letter")
+        @Pattern(regexp = ".*[a-z].*", message = "Password have to contain a least one lowercase letter")
+        @Pattern(regexp = ".*[0-9].*", message = "Password have to contain a least one number")
         String password,
 
-        @Pattern(regexp = "^(?:\\+46\\s?7\\d-\\d{7}|07\\d-\\d{7}|\\+46\\d{1,3}-\\d{5,8}|0\\d{1,3}-\\d{5,8})$\n"
+        @Pattern(regexp = "^(?:\\+46\\s?\\d{1,3}-\\d{5,8}|0\\d{1,3}-\\d{5,8})$\n"
                 , message = "Phone number to be in phone or mobile format, for example xxx-xxxxxxx.")
         String phoneNumber
 ) {
