2aca54f13a5eb7aacb6abc9b045ea4b4c5aadb07
soppitopp <soppitopp2000@hotmail.com>
2026-05-20 19:57:36 +0200

    add @Size

--- src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
@@ -18,6 +18,7 @@ public record CreateCustomerRequest(
         String email,
 
         @NotBlank(message = "You must enter a password.")
+        @Size(min = 10, message = "password needs to be at least 10 character long")
         @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "Password have to contain a least one special character")
         @Pattern(regexp = ".*[A-Z].*", message = "Password have to contain a least one uppercase letter")
         @Pattern(regexp = ".*[a-z].*", message = "Password have to contain a least one lowercase letter")
