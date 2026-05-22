06083001b95fc8a6168b372d4e2b63a7e47947d3
soppitopp <soppitopp2000@hotmail.com>
2026-05-22 11:16:16 +0200

    removed CustomerExistException.java

--- src/main/java/org/example/bookingsystem/exceptionhandler/customexeptions/CustomerExistException.java
+++
@@ -1,7 +0,0 @@
-package org.example.bookingsystem.exceptionhandler.customexeptions;
-
-public class CustomerExistException extends RuntimeException{
-    public CustomerExistException(String message) {
-        super(message);
-    }
-}
