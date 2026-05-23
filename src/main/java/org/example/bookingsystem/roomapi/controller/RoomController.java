0cbaefad3a516357986788f999ef4687006b2dca
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-23 05:29:52 +0200

    removed redundant import

--- src/main/java/org/example/bookingsystem/roomapi/controller/RoomController.java
+++ src/main/java/org/example/bookingsystem/roomapi/controller/RoomController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.example.bookingsystem.roomapi.dto.AddNewRoomDto;
 import org.example.bookingsystem.roomapi.dto.RoomResponseDto;
 import org.example.bookingsystem.roomapi.dto.UpdateRoomDto;
-import org.example.bookingsystem.roomapi.entity.Room;
 import org.example.bookingsystem.roomapi.service.RoomService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
