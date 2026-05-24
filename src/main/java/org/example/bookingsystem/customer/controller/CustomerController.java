e334e13b96f9edafb2a1d82b119ff69e657394c5
soppitopp <soppitopp2000@hotmail.com>
2026-05-24 15:48:51 +0200

    Reformat CustomerController.java

--- src/main/java/org/example/bookingsystem/customer/controller/CustomerController.java
+++ src/main/java/org/example/bookingsystem/customer/controller/CustomerController.java
@@ -1,15 +1,21 @@
 package org.example.bookingsystem.customer.controller;
 
-import jakarta.validation.*;
-import org.example.bookingsystem.customer.model.*;
-import org.example.bookingsystem.customer.model.dto.*;
-import org.example.bookingsystem.customer.service.*;
+import jakarta.validation.Valid;
+
+import org.example.bookingsystem.customer.model.Customer;
+import org.example.bookingsystem.customer.model.dto.CreateCustomerRequest;
+import org.example.bookingsystem.customer.model.dto.CustomerLoginRequest;
+import org.example.bookingsystem.customer.model.dto.CustomerUpdateRequest;
+import org.example.bookingsystem.customer.service.CustomerService;
 import org.example.bookingsystem.security.jwt.model.CustomUserDetails;
 import org.example.bookingsystem.security.jwt.model.dto.JwtResponse;
-import org.example.bookingsystem.security.jwt.service.*;
-import org.springframework.http.*;
+import org.example.bookingsystem.security.jwt.service.JwtService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("api/customers")
 public class CustomerController {
@@ -31,8 +37,8 @@ public class CustomerController {
      * The password is checked against the database (via customerService + PasswordEncoder).
      * If that is correct, you will get a Customer object.
      * You call jwtService.generateToken(...) with:
-        * customerId → is placed as the subject
-        * email → is added as claim "email"
+     * customerId → is placed as the subject
+     * email → is added as claim "email"
      * JwtService signs the token with your secret key (jwt.secret) and sets the expiration time.
      * Result: the client (frontend / Postman / Thunder client) receives a JWT string back (a token that is stored in the client).
      * For each protected request, the header is sent: Authorization: Bearer <your-token-here>
@@ -47,9 +53,7 @@ public class CustomerController {
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<?> updateCustomer(
-            @AuthenticationPrincipal CustomUserDetails user,
-            @RequestBody CustomerUpdateRequest request) {
+    public ResponseEntity<?> updateCustomer(@AuthenticationPrincipal CustomUserDetails user, @RequestBody CustomerUpdateRequest request) {
 
         Long id = user.getId();
 
@@ -58,6 +62,7 @@ public class CustomerController {
         }
 
         customerService.updateCustomerInfo(id, request);
+
         return ResponseEntity.noContent().build();
     }
 }
