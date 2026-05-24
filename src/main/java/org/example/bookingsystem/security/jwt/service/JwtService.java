fe9c145d199a5d7525dc958e6496e953ae1f3a23
soppitopp <soppitopp2000@hotmail.com>
2026-05-24 13:29:59 +0200

    add comments to JwtService

--- src/main/java/org/example/bookingsystem/security/jwt/service/JwtService.java
+++ src/main/java/org/example/bookingsystem/security/jwt/service/JwtService.java
@@ -8,14 +8,33 @@ import org.springframework.stereotype.*;
 import java.security.*;
 import java.util.*;
 
+/*
+* @Service makes the class a Spring bean.
+* It is responsible for creating, signing, and reading JWT tokens.
+*/
 @Service
 public class JwtService {
     private final Key key;
 
+    /*
+    * You are reading jwt.secret from application.properties.
+    * You convert it into an HMAC SHA key.
+    * Keys.hmacShaKeyFor(...) requires at least 32 characters for HS256/HS512.
+     */
     public JwtService(@Value("${jwt.secret}") String secret) {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /*
+    * Creating JWT.
+    * .setSubject(customerId.toString()) -> This is the standard field for 'who is the user?'.
+    * .claim("email", email) -> You add email as extra data in the token.
+    * .setIssuedAt(new Date()) -> sets when the token was created.
+    * .setExpiration(...) -> sets when token expiration.
+        * Token is valid for 1 hour!
+    * .signWith(key, SignatureAlgorithm.HS512) -> You sign the token with your secret key.
+    * .compact(); -> A compact JWT string that the client receives at login.
+     */
     public String generateToken(Long customerId, String email) {
         return Jwts.builder()
                 .setSubject(customerId.toString())
@@ -26,6 +45,12 @@ public class JwtService {
                 .compact();
     }
 
+    /*
+     * You build a parser with your signing key.
+     * You validate the token's signature, expiration, and format.
+     * You retrieve the subject, which is customerId.
+     * All exceptions are caught in JwtAuthFilter.
+     */
     public Long extractCustomerId(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
@@ -35,6 +60,12 @@ public class JwtService {
         return Long.parseLong(claims.getSubject());
     }
 
+    /*
+     * You build a parser with your signing key.
+     * You validate the token's signature, expiration, and format.
+     * You retrieve the claim, which is email.
+     * All exceptions are caught in JwtAuthFilter.
+     */
     public String extractEmail(String token) {
         Claims claims = Jwts.parserBuilder()
                 .setSigningKey(key)
