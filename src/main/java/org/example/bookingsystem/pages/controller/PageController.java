9d4bbea98f5c8c4b302c99b4bfc96a4b851381cc
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-25 16:14:26 +0200

    Page controller to fetch a page

---
+++ src/main/java/org/example/bookingsystem/pages/controller/PageController.java
@@ -0,0 +1,31 @@
+package org.example.bookingsystem.pages.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "MyPage";
+    }
+
+
+
+}
