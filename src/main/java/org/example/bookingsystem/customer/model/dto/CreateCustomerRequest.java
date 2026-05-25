0299bb139d3f00a5128ce4363aa3ee3df8384505
soppitopp <soppitopp2000@hotmail.com>
2026-05-25 19:30:05 +0200

    fix format

--- src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
@@ -25,7 +25,8 @@ public record CreateCustomerRequest(
         @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "Password have to contain a least one special character")
         @Pattern(regexp = ".*[A-Z].*", message = "Password have to contain a least one uppercase letter")
         @Pattern(regexp = ".*[a-z].*", message = "Password have to contain a least one lowercase letter")
-        @Pattern(regexp = ".*[0-9].*", message = "Password have to contain a least one number") String password,
+        @Pattern(regexp = ".*[0-9].*", message = "Password have to contain a least one number")
+        String password,
 
         @Pattern(regexp = "^(?:\\+46\\s?7\\d-\\d{7}|07\\d-\\d{7}|\\+46\\d{1,3}-\\d{5,8}|0\\d{1,3}-\\d{5,8})$",
                 message = "Phone number to be in phone or mobile format, for example xxx-xxxxxxx.")
