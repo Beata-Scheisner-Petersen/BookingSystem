4cbe460403a04a1b0fc0ab118a37839539b15852
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-25 15:30:52 +0200

    Created a login function to login user

--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,22 +1,17 @@
-document.getElementById("login_form")
-    .addEventListener("submit", async function (event) {
-        event.preventDefault();
+async function login() {
 
-        const email = document.getElementById("email_input").value;
-        const password = document.getElementById("password_input").value;
+    const email = document.getElementById("email_input").value;
+    const password = document.getElementById("password_input").value;
 
-        const response = await fetch("/api/customers/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, password })
-        });
-
-        if (response.ok) {
-            const jwtData = await response.json();
-            window.localStorage.setItem("jwt", jwtData.token);
-
-            window.location.href = "static/MyPage.html";
-        } else {
-            alert("Login failed");
-        }
+    const response = await fetch("/api/customers/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
     });
+
+    if (response.ok) {
+        window.location.href = "/mypage";
+    } else {
+        alert("Login failed");
+    }
+}
