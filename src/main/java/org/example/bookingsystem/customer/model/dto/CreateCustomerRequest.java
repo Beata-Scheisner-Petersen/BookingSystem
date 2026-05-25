da6c299e089af15282fe57f07fee78fe01fd61b5
soppitopp <soppitopp2000@hotmail.com>
2026-05-25 09:24:31 +0200

    Reformat CreateCustomerRequest.java

--- src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CreateCustomerRequest.java
@@ -29,5 +29,6 @@ public record CreateCustomerRequest(
 
         @Pattern(regexp = "^(?:\\+46\\s?7\\d-\\d{7}|07\\d-\\d{7}|\\+46\\d{1,3}-\\d{5,8}|0\\d{1,3}-\\d{5,8})$",
                 message = "Phone number to be in phone or mobile format, for example xxx-xxxxxxx.")
-        String phoneNumber) {
+        String phoneNumber
+) {
 }
