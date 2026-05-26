8c5138b51f32ebc48a5ced5ef7810b9adb06ca59
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-26 13:57:53 +0200

    Refactored some annotation messages and corrected spelling

--- src/main/java/org/example/bookingsystem/customer/model/dto/CustomerLoginRequest.java
+++ src/main/java/org/example/bookingsystem/customer/model/dto/CustomerLoginRequest.java
@@ -6,15 +6,11 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CustomerLoginRequest(
-        @NotBlank(message = "You must enter a email.")
-        @Email(message = "Email format is invalid.")
+        @NotBlank(message = "You must enter an email.")
+        @Email(message = "Not a valid email.")
         String email,
 
         @NotBlank(message = "You must enter a password.")
-        @Size(min = 10, message = "password needs to be at least 10 character long")
-        @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "Password have to contain a least one special character")
-        @Pattern(regexp = ".*[A-Z].*", message = "Password have to contain a least one uppercase letter")
-        @Pattern(regexp = ".*[a-z].*", message = "Password have to contain a least one lowercase letter")
-        @Pattern(regexp = ".*[0-9].*", message = "Password have to contain a least one number")
+
         String password) {
 }
