57cca112adda619bfa69b1974ab3bcd39029c17e
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-05-28 03:00:17 +0200

    Cleaned up for better readability

--- src/main/java/org/example/bookingsystem/roomapi/dto/RoomResponseDto.java
+++ src/main/java/org/example/bookingsystem/roomapi/dto/RoomResponseDto.java
@@ -2,6 +2,11 @@ package org.example.bookingsystem.roomapi.dto;
 
 import java.math.BigDecimal;
 
-public record RoomResponseDto(Long id, int roomNumber, String roomType, BigDecimal roomPrice, int maxGuests,
-                              boolean extraBedAvailable) {
+public record RoomResponseDto(
+        Long id,
+        int roomNumber,
+        String roomType,
+        BigDecimal roomPrice,
+        int maxGuests,
+        boolean extraBedAvailable) {
 }
