dcc0dbcb392c78453ebbab67f83355cd33e6fb9c
soppitopp <soppitopp2000@hotmail.com>
2026-06-13 09:08:13 +0200

    fix: removed unused imports and parameter

--- src/main/java/org/example/bookingsystem/pages/service/PageService.java
+++ src/main/java/org/example/bookingsystem/pages/service/PageService.java
@@ -1,20 +1,15 @@
 package org.example.bookingsystem.pages.service;
 
-import org.example.bookingsystem.customer.model.Customer;
 import org.example.bookingsystem.customer.model.dto.CustomerInfoRequest;
 import org.example.bookingsystem.customer.repository.CustomerRepository;
 import org.example.bookingsystem.exceptionhandler.customexeptions.NotFoundException;
-import org.example.bookingsystem.reservation.model.GetReservationInfo;
-import org.example.bookingsystem.reservation.repository.ReservationRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class PageService {
     private final CustomerRepository customerRepository;
 
-    public PageService(CustomerRepository customerRepository, ReservationRepository reservationRepository) {
+    public PageService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
